a58fde27c19fbbc5524c2e418294b6a4f3386498
koumura2501 <koumura2501@gmail.com>
2020-03-30 23:00:10 +0900

    make generators

---
+++ config/initializers/generators.rb
@@ -0,0 +1,14 @@
+Rails.application.config.generators do |g|
+  g.skip_routes true
+  g.assets true
+  g.stylesheets false
+  g.helper false
+  g.template_engine :erb
+  g.test_framework :rspec,
+                   fixtures: true,
+                   view_specs: false,
+                   routing_specs: false,
+                   helper_specs: false,
+                   controller_specs: false,
+                   request_specs: true
+end
