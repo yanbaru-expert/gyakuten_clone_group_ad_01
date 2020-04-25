6074ad254d88a0cce379709558ff77e1ed8c9ae3
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-04-25 23:16:43 +0900

    execute rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,21 @@
 module ApplicationHelper
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div(:line_numbers => :table)
+      CodeRay.scan(code, language).div(line_numbers: :table)
     end
   end
 
   def markdown(text)
-    coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
     options = {
-      :fenced_code_blocks => true, #ここのoptionは任意で好きなようにカスタマイズしてok
-      :no_intra_emphasis  => true,
-      :autolink           => true,
-      :strikethrough      => true,
-      :lax_html_blocks    => true,
-      :superscript        => true
-     }
-     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-     markdown_to_html.render(text).html_safe
+      fenced_code_blocks: true, # ここのoptionは任意で好きなようにカスタマイズしてok
+      no_intra_emphasis: true,
+      autolink: true,
+      strikethrough: true,
+      lax_html_blocks: true,
+      superscript: true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 end
