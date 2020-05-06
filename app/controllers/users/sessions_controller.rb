5313c51dfad009d7050e8c758a57cb31ec1b457e
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-05-06 22:09:12 +0900

    repair session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,4 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  before_action :user_flag_check
-
-  private
-
-  def user_flag_check
-    if user_signed_in? && (current_user.user_flag == false)
-      sign_out
-      flash[:alert] = "現在利用者でないためログインできません。管理者に問い合わせてください。"
-      redirect_to new_user_session_path
-    end
-  end
 end
