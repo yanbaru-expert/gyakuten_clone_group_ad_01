fbca5a23583a5ce5d95354394d1c9486b4069e5f
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-05-10 09:09:03 +0900

    repair password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,13 +61,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  # for SMTP
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "127.0.0.1",
-    domain: "example.com",
-    port: 25
-  }
 
   config.action_mailer.delivery_method = :letter_opener_web
 end
