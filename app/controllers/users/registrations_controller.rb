7506351b7cc87dabff87fe27254de514b050f391
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-05-14 05:32:09 +0900

    repair controller

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, only: [:update]
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-  end
-end
