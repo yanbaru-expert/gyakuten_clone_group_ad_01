12a39d50803c5f55eb9c8462470721e125e39e1b
hayato sato <hayatosato@hayato-MacBook-Pro.local>
2020-05-27 08:28:35 +0900

    deploy_retry

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,11 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Amazon S3用の設定
-      provider: 'AWS',
-      region: 'ap-northeast-1',
-      aws_access_key_id:  'AKIAWYHIWN7UROT75V4P',
-      aws_secret_access_key:  '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'
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['gyakuten-salon-ad-01-bucket']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
