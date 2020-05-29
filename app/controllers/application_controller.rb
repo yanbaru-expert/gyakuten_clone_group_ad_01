af0ab6006d91f5e762aa87d050dedc040b4a6f8d
T.Tsujii <ttakehisabt@gmail.com>
2020-05-29 11:40:31 +0900

    Heroku本番環境でエラーが発生する問題を解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # 全ページをログイン必須とする
   before_action :authenticate_user!
-  before_action :set_host
+  # before_action :set_host
   before_action :configure_permitted_parameters, only: [:update], if: :devise_controller?
 
   private
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
-  def set_host
-    Rails.application.routes.default_url_options[:host] = request.host_with_port
-  end
+  # def set_host
+  #   Rails.application.routes.default_url_options[:host] = request.host_with_port
+  # end
 end
