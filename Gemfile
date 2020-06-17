e217e435d0bc5c265976c32ea5e86038896d859e
Toshiaki Kuramochi <toshi083073@gmail.com>
2020-06-17 12:47:37 +0900

    heroku-staging

--- Gemfile
+++ Gemfile
@@ -48,3 +48,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+group :production do
+  gem "pg", ">= 0.18", "< 2.0"
+end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,6 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Amazon S3用の設定
-<<<<<<< HEAD
       :provider              => 'AWS',
       :region                => ENV['S3_ap-northeast-1'],     # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_AKIAQH4RXPRSXRXKXVG3'],
@@ -11,13 +10,3 @@ if Rails.env.production?
     config.fog_directory     =  ENV['S3_healthmanagementapp']
   end
 end
-=======
-      provider: "AWS",
-      region: ENV["S3_REGION"],
-      aws_access_key_id: ENV["S3_ACCESS_KEY"],
-      aws_secret_access_key: ENV["S3_SECRET_KEY"]
-    }
-    config.fog_directory = ENV["S3_BUCKET"]
-  end
-end
->>>>>>> 3ae0c64efab9cc7114b416d95d29e0171fac0b59
